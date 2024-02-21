76ba97f007db07fc1d0696e0a55e413d5f7a0a3b
Fadime Aydiner <fadime_aydiner@icloud.com>
2024-02-21 11:34:43 -0600

    Bottom Navigator Created

--- components/Onboard/Slides.jsx
+++ components/Onboard/Slides.jsx
@@ -1,10 +1,15 @@
 import { View, Image} from 'react-native'
 import React from 'react'
 import styles from './Slides.style'
-import {HeightSpacer, ResuableBtn, ReusableText} from '../../components/index'
+import {HeightSpacer,
+       ResuableBtn, 
+       ReusableText
+       } from '../../components/index'
 import { COLORS, SIZES } from '../../constants/theme'
+import { useNavigation } from '@react-navigation/native'
 
 const Slides = ({item}) => {
+  const navigation=useNavigation();
   return (
     <View>
       <Image source={item.image} style={styles.image}/>
@@ -18,16 +23,16 @@ const Slides = ({item}) => {
             />
             <HeightSpacer height={40}/>
             <ResuableBtn 
-                onPress={()=>{}}
+                onPress={()=>navigation.navigate('Bottom')}
                 btnText={"Let's go"}
                 width={(SIZES.width-50)/2.2}
                 backgroundColor={COLORS.red}
                 borderColor={COLORS.red}
                 borderWidth={0}
                 textColor={COLORS.white}
-
-            
+          
             />
+            
       </View>
     </View>
   );
---
+++ screens/home/Home.jsx
@@ -0,0 +1,46 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import reusable from '../../components/Reusable/reusable.style'
+import { HeightSpacer, ReusableText } from '../../components'
+import { COLORS, SIZES, TEXT } from '../../constants/theme'
+import {AntDesign} from "@expo/vector-icons"
+import styles from './home.style'
+
+
+const Home = ({navigation}) => {
+  return (
+    <SafeAreaView style={reusable.container}>
+      <View>
+        <View style={reusable.rowWithSpace("space-between")}>
+        <ReusableText
+                text={"Hey User"}
+                family={'regular'}
+                size={TEXT.large}
+                color={COLORS.white}
+         />
+
+         <TouchableOpacity style={styles.box}
+          onPress={()=>navigation.navigate('Search')}
+         >
+          <AntDesign
+            name='search1'
+            size={26}
+          />
+
+         </TouchableOpacity>
+        </View>
+        <HeightSpacer height={SIZES.xLarge}/>
+        <ReusableText
+                text={"Places"}
+                family={"medium"}
+                size={TEXT.large}
+                color={COLORS.white}
+         />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default Home
+
